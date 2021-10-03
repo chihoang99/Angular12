c790fe59981414afb93e0cfa8433dec0aef215ac
chihoang99 <hoangdang240299@gmail.com>
2021-10-03 20:09:53 +0700

    DataBindingValue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public name = 'Hoàng Đặng';
+  public age = 25;
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quýt', 'Xoài'];
+  public traiCay2 = [
+    {ten: 'Tao', gia: 12, haGia: true},
+    {ten: 'Nho', gia: -3.321, haGia: false},
+    {ten: 'Cam', gia: 34.32, haGia: false},
+    {ten: 'Quýt', gia: 32.31, haGia: true},
+    ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    console.log('trai cay =', this.traiCay2)
+   }
+
+  public resetName(): void{
+    console.log('resetName()');
+    this.name = '';
   }
+ 
 
 }
