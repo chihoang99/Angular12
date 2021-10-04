068fa9ef7cb72ad3521b40998e2a3336b2e630a0
chihoang99 <hoangdang240299@gmail.com>
2021-10-04 13:11:48 +0700

    Combobox TP_Quan_ Huyen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,44 @@ export class HomeComponent implements OnInit {
     {ten: 'Cam', gia: 34.32, haGia: false},
     {ten: 'Quýt', gia: 32.31, haGia: true},
     ];
+  public districts: string[] = ['Quận/Huyện'];
+  public vietnamData = [
+    {city: 'Chọn thành phố', district: ['Quận/Huyện']},
+    {
+      city: 'An Giang',
+      district:[
+        'Thành phố Long Xuyên',
+        'Thành phố Châu Đốc',
+        'Thị xã Tân Châu',
+      ], 
+    },
+    {
+      city: 'Bà Rịa - Vũng Tàu',
+      district:[
+        'Thành phố Vũng Tàu',
+        'Thị xã Bà Rịa',
+        'Thị xã Phú Mỹ',
+      ], 
+    },
+    {
+      city: 'Bạc Liêu',
+      district:[
+        'Thành phố Bạc Liêu',
+        'Huyện Đông Hải',
+        'Huyện Giá Rai',
+      ], 
+    },
+    {
+      city: 'Bắc Kạn',
+      district:[
+        'Thành phố Bắc Kạn',
+        'Huyện Ba Bể',
+        'Huyện Bạch Thông',
+      ], 
+    },
+  ];
+
+  
 
   constructor() { }
 
@@ -25,6 +63,22 @@ export class HomeComponent implements OnInit {
   public resetName(): void{
     console.log('resetName()');
     this.name = '';
+  }
+  public changeCity(event: any): void{
+    const city = event.target.value;
+    if(!city){
+      return;
+    }
+
+    //Cách 1
+    // const search = this.vietnamData.filter(data => data.city === city);
+    // console.log('search', search);
+    // if(search && search.length > 0){
+    //   this.districts = search[0].district;
+    // }
+
+    //Cách 2
+    this.districts = this.vietnamData.find(data => data.city === city)?.district || [];
   }
  
 
